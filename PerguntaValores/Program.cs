d9bff6b53a1db1f2895f69a25d0b4f59ea2b0ca5
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-03-10 14:29:15 +0000

    Add cylinder's area and radius

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the cylinder's height:");
+            string areaquestion = Console.ReadLine();
+            Console.WriteLine("Enter the cylinder's radius:");
+            string radiusquestion = Console.ReadLine();
+
+            int area = Convert.ToInt32(areaquestion);
+            int radius = Convert.ToInt32(radiusquestion);
+
+            float pi = 3.1415926f;
+            float V = pi * radius * radius * area;
+
+            Console.WriteLine($"The cylinder's volume is: {V}");
+        }
+    }
+}
