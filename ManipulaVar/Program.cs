f252ed25ea82953e785f402741bde35e99081988
rodrigogomes01 <115220374+rodrigogomes01@users.noreply.github.com>
2023-03-10 14:06:45 +0000

    First commit

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ManipulaVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(args[0]);
+            x++;
+            Console.WriteLine(x);
+        }
+    }
+}
